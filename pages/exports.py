a3b55aa5344dc7cccff965faf119f2d299b91acb
pranaavs2003 <70879706+pranaavs2003@users.noreply.github.com>
2024-02-08 12:45:07 +0000

    Changes Commit

--- pages/exports.py
+++ pages/exports.py
@@ -9,7 +9,7 @@ def group_by_route(df):
     workbook = xlsxwriter.Workbook(output, {'in_memory': True})
 
     for i in route_names:
-        worksheet = workbook.add_worksheet(name=i)
+        worksheet = workbook.add_worksheet(name=i[:30])
         #worksheet.write('A1', 'semester')
         worksheet.write('A1', 'route_name')
         worksheet.write('B1', 'registration_number')
@@ -69,7 +69,7 @@ def combined(df):
     group_by_route_name = df.groupby('destination')
 
     for i in route_names:
-        worksheet = workbook.add_worksheet(name=i)
+        worksheet = workbook.add_worksheet(name=i[:30])
         #worksheet.write('A1', 'semester')
         worksheet.write('A1', 'route_name')
         worksheet.write('B1', 'registration_number')
